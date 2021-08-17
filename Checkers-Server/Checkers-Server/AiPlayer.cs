d93a46cbf306345fcd4c12918b04d63b63a9192e
Unknown <wolfj@post.bgu.ac.il>
2021-08-17 18:42:08 +0300

    ...

--- Checkers-Server/Checkers-Server/AiPlayer.cs
+++ Checkers-Server/Checkers-Server/AiPlayer.cs
@@ -4,8 +4,27 @@ using System.Text;
 
 namespace Checkers_Server
 {
-    class AiPlayer : IPlayer 
+    class AiPlayer : IPlayer
     {
         //TODO: AiPlayer
+        public Color? GetColor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyWinners(List<IPlayer> winners)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetColor(Color? color)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
